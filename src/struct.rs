54b705c6462aa4f421b15488a822e6452c55eb89
four <ai-l@outlook.com>
2020-05-08 00:17:51 +0800

    1

---
+++ src/struct.rs
@@ -0,0 +1,21 @@
+#![allow(unused_variables)]
+
+fn main() {
+    struct User {
+        username: String ,
+        email:  String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.username = String::from("four");
+    println!("{}{}",user1.username,user1.email)
+
+}
