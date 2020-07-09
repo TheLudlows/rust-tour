34ec998bfb8e1df107d5a52ea405187d8dc1692a
four <ai-l@outlook.com>
2020-07-10 00:10:21 +0800

    1

--- algorithm/src/leetcode/mod.rs
+++ algorithm/src/leetcode/mod.rs
@@ -1,3 +1,4 @@
+mod p0003_longest_sub_string;
 mod p0008_str_int;
 mod p0002_add_two_num;
 mod p0001_two_sum;
---
+++ algorithm/src/leetcode/p0003_longest_sub_string.rs
@@ -0,0 +1,16 @@
+struct Solution;
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut max_len  = 0;
+            s.
+        0
+    }
+    fn indexOf(arr:&[char], c:char, start:usize,end:usize) -> i32 {
+        for i in start..=end {
+            if arr[i] == c {
+               return  i as i32
+            }
+        }
+        -1 as i32
+    }
+}
