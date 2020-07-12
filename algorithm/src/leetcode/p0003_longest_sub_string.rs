79d6878e145353b9419cc476903366cefc41d723
four <ai-l@outlook.com>
2020-07-12 19:21:46 +0800

    1

--- algorithm/src/leetcode/p0003_longest_sub_string.rs
+++ algorithm/src/leetcode/p0003_longest_sub_string.rs
@@ -2,7 +2,6 @@ struct Solution;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut max_len  = 0;
-            s.
         0
     }
     fn indexOf(arr:&[char], c:char, start:usize,end:usize) -> i32 {
