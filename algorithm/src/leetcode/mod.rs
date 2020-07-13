c0af95c89ce0fe8d4531b14584651ebf94ec0c2f
four <ai-l@outlook.com>
2020-07-14 00:06:10 +0800

    1

--- algorithm/src/leetcode/mod.rs
+++ algorithm/src/leetcode/mod.rs
@@ -1,3 +1,4 @@
+mod p0004_mid;
 mod p0003_longest_sub_string;
 mod p0008_str_int;
 mod p0002_add_two_num;
---
+++ algorithm/src/leetcode/p0004_mid.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let mut v1 = nums1;
+        let mut v2 = nums2;
+        v1.append(&mut v2);
+        v1.sort();
+        let len = v1.len();
+        return if len % 2 == 0 {
+            ((v1[len / 2] + v1[len / 2 - 1]) as f64 / 2.0) as f64
+        } else {
+            v1[len / 2] as f64
+        }
+    }
+}
+struct Solution;
+#[test]
+fn test() {
+
+}
