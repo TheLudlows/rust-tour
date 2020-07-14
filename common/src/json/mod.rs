b9fbec1370c68d6153a1a6963c7f211406ae342f
liuchao56 <liuchao56@jd.com>
2020-07-14 20:35:59 +0800

    -

---
+++ common/src/json/mod.rs
@@ -0,0 +1,10 @@
+pub struct Foo;
+impl Foo{
+    pub fn say_hello(&self) {
+        println!("hello")
+    }
+    pub fn new() -> Self{
+        Foo
+    }
+}
+pub type Boo =  Foo;
--- common/src/main.rs
+++ common/src/main.rs
@@ -1,17 +1,5 @@
 use compressed_vec::VectorU32Appender;
-mod compresion;
-mod json;
 
-
-struct Foo;
-type Boo =  Foo;
-impl Foo{
-    pub fn say_hello(&self) {
-        println!("hello")
-    }
-}
 fn main() {
-
-    let b = Boo;
-    b.
+    let v = VectorU32Appender::tr
 }
