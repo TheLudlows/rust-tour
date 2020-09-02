5dc810042edc1fd9da2b3cf46909ee650606cbe3
four <ai-l@outlook.com>
2020-09-02 23:21:29 +0800

    update

--- src/any.rs
+++ src/any.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::any::{Any, TypeId};
 
+#[test]
+fn test() {
+    struct Foo {
+        x: u8,
+        y: u8,
+    }
+    let f = Foo { x: 0, y: 10 };
+    let str = "abc";
+    let num = 200;
+    let pointer = &num;
+    println!("{:?}", (&f).type_id());
+    println!("{:?}", f.type_id());
+
+    println!("{:?}", (&dyn Any) f.type_id());
 }
--- src/closures.rs
+++ src/closures.rs
@@ -9,3 +9,20 @@ fn closure_test() {
     assert!(equal_to_x(y));
 }
 
+
+fn returns_closure0() -> Box<Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn returns_closure1() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn returns_closure2() -> Box<impl Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn returns_closure3() -> impl Fn(i32) -> i32 {
+    |x| x + 1
+}
+
