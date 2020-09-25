3853a3d5c6a14dc3793a7c9883964f30777b1eab
four <ai-l@outlook.com>
2020-09-25 18:54:43 +0800

    update

--- algorithm/src/leetcode/p0004_find_median_sorted_arrays.rs
+++ algorithm/src/leetcode/p0004_find_median_sorted_arrays.rs
@@ -11,7 +11,7 @@ impl Solution {
         let mut i = 0;
         let mut j = 0;
         let mut cur: i32 = 0;
-        while  i < nums1.len() || j < nums2.len() {
+        while i < nums1.len() || j < nums2.len() {
             // 如果第二个数组已经遍历完，只能选择第一个数组
             if j == nums2.len() || (i < nums1.len() && nums1[i] < nums2[j]) {
                 cur = nums1[i];
@@ -30,6 +30,30 @@ impl Solution {
         }
         res / 2.0
     }
+    pub fn find_median_sorted_arrays_2(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> f64 {
+        let min_left = (nums1.len() + nums2.len() + 1) / 2;
+        let min_right = (nums1.len() + nums2.len() + 2) / 2;
+        (findK(&nums1, 0, &nums2, 0, min_left) + findK(&nums1, 0, &nums2, 0, min_right)) as f64 / 2.0
+    }
+}
+
+fn findK(v1: &Vec<i32>, i: usize, v2: &Vec<i32>, j: usize, k: usize) -> i32 {
+    if i >= v1.len() {
+        return v2[j + k - 1];
+    }
+    if j >= v2.len() {
+        return v1[i + k - 1];
+    }
+    if k == 1 {
+        return min(v1[i], v2[j]);
+    }
+    let max1 = if (i + k - 1) < v1.len() { v1[i + k - 1] } else { i32::MAX };
+    let max2 = if (j + k - 1) < v2.len() { v2[j + k - 1] } else { i32::MAX };
+    return if max1 > max2 {
+        findK(v1, i, v2, j + k / 2, k - k / 2)
+    } else {
+        findK(v1, i + k / 2, v2, j, k - k / 2)
+    }
 }
 
 #[test]
@@ -37,22 +61,10 @@ fn test() {
     let v1 = vec![1, 2];
     let v2 = vec![3, 4];
 
-    let r = Solution::find_median_sorted_arrays(v1, v2);
+    let r = Solution::find_median_sorted_arrays_2(v1, v2);
 
     println!("{}", r)
 }
-/// JAVA
-// class Solution {
-//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-//         int len1 = nums1.length;
-//         int len2 = nums2.length;
-//         int total = len1 + len2;
-//         int left = (total + 1) / 2;
-//         int right = (total + 2) / 2;
-//         return (findK(nums1, 0, nums2, 0, left) + findK(nums1, 0, nums2, 0, right)) / 2.0;
-//
-//     }
-//
 //     //找到两个数组中第k小的元素
 //     public int findK(int[] nums1, int i, int[] nums2, int j, int k) {
 //         if (i >= nums1.length)
