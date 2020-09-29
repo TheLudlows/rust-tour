b36d92225d0a044a54bb76642cf4ec4c23cce478
four <ai-l@outlook.com>
2020-09-29 23:37:19 +0800

    update

--- algorithm/src/leetcode/p0046_permute.rs
+++ algorithm/src/leetcode/p0046_permute.rs
@@ -1,23 +1,27 @@
 use crate::leetcode::common::Solution;
-
+/// 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut result: Vec<Vec<i32>> = vec![];
         let mut arr: Vec<i32> = vec![];
-        Solution::backtrace(&nums, &mut arr, &mut result);
+        let mut vis = vec![false;nums.len()];
+        backtrace(&nums, &mut arr, &mut result,&mut vis);
         return result;
     }
 
-    fn backtrace(nums: &Vec<i32>, arr: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
-        if arr.len() == nums.len() {
-            result.push(arr.clone());
-        } else {
-            for i in nums {
-                if !arr.contains(i) {
-                    arr.push(*i);
-                    Solution::backtrace(nums, arr, result);
-                    arr.pop();
-                }
+}
+
+fn backtrace(nums: &Vec<i32>, arr: &mut Vec<i32>, result: &mut Vec<Vec<i32>>,vis:&mut Vec<bool>) {
+    if arr.len() == nums.len() {
+        result.push(arr.clone());
+    } else {
+        for i in 0..nums.len() {
+            if !vis[i] {
+                vis[i] = true;
+                arr.push(nums[i]);
+                backtrace(nums, arr, result,vis);
+                vis[i] = false;
+                arr.pop();
             }
         }
     }
