9184af7bf08fc63872a59178ab329e868c77ae8b
four <ai-l@outlook.com>
2020-11-14 19:08:18 +0800

    update

--- algorithm/src/sort/mod.rs
+++ algorithm/src/sort/mod.rs
@@ -3,3 +3,4 @@ mod p0056_merge;
 mod p0057_merge2;
 mod p0075_sort_colors;
 mod p0912_quick_sort;
+mod p1122_relative_sort_array;
---
+++ algorithm/src/sort/p1122_relative_sort_array.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+use crate::Solution;
+
+impl Solution {
+    pub fn relative_sort_array(mut arr1: Vec<i32>, arr2: Vec<i32>) -> Vec<i32> {
+        let map = arr2.into_iter().enumerate().map(|(i, x)| (x, i)).collect::<HashMap<i32, usize>>();
+        arr1.sort_by_key(|e| map.get(e).cloned().unwrap_or((1000 + *e as usize)));
+        arr1
+    }
+}
