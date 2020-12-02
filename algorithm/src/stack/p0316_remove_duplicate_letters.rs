9e5f68c3df97427d0a8a567c28f811fac2b6c8fc
four <ai-l@outlook.com>
2020-12-03 00:03:37 +0800

    update

--- algorithm/src/stack/p0316_remove_duplicate_letters.rs
+++ algorithm/src/stack/p0316_remove_duplicate_letters.rs
@@ -4,11 +4,24 @@ use std::collections::{HashSet, HashMap};
 impl Solution {
     pub fn remove_duplicate_letters(s: String) -> String {
         let mut result = String::new();
+        let mut map = HashMap::new();
+        s.chars().enumerate().for_each(|(i,c)| {map.insert(c,i);});
+        let mut set:HashSet<char> = HashSet::new();
+        for (i,c) in s.chars().enumerate() {
+            if !set.contains(&c) {
+                while !result.is_empty() && result.chars().last().unwrap() > c && *map.get(&result.chars().last().unwrap()).unwrap() > i {
+                    set.remove(&result.pop().unwrap());
+                }
+                result.push(c);
+                set.insert(c);
+            }
+        }
         result
     }
 }
 
 #[test]
 fn test() {
-    Solution::remove_duplicate_letters("bcabc".to_string());
+    let r = Solution::remove_duplicate_letters("bcabc".to_string());
+    println!("{}",r)
 }
