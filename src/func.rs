31c3de67d47840ee9f491bc8c2d62355dcab49bd
four <ai-l@outlook.com>
2020-12-29 19:21:22 +0800

    update

--- src/func.rs
+++ src/func.rs
@@ -36,7 +36,7 @@ fn add_test() {
     let r = square::<f32>(4.2, 5.0);
 }
 
-fn square<T: Mul<T, Output = T>>(x: T, y: T) -> T {
+fn square<T: Mul<T, Output=T>>(x: T, y: T) -> T {
     x * y
 }
 
@@ -56,4 +56,31 @@ fn iif(flag: bool) -> &'static str {
 fn test_fn() {
     println!("{}", ya_iif(false));
     println!("{}", iif(true));
+}
+
+#[test]
+fn test_fn_ref() {
+    #![allow(unused)]
+    trait Trait {
+        fn f(self);
+    }
+
+    impl<T> Trait for fn(T) {
+        fn f(self) {
+            print!("1");
+        }
+    }
+
+    impl<T> Trait for fn(&T) {
+        fn f(self) {
+            print!("2");
+        }
+    }
+
+    let a : fn(t:i32) = |i: i32| {};
+    let b  : fn(_)= |_: &u8| {};
+    let c  : fn(&_)= |_: &u8| {};
+    a.f();
+    b.f();
+    c.f();
 }
