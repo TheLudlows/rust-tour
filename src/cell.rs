2b935c051099bae5b456b5170e81abad51c946b5
four <ai-l@outlook.com>
2020-12-31 19:01:31 +0800

    update

--- src/cell.rs
+++ src/cell.rs
@@ -1,4 +1,6 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[test]
 fn cell_test() {
@@ -15,4 +17,18 @@ fn cell_test() {
     f.b.set(30);
     assert_eq!(30, f.b.get());
     f.c.set("ccc".to_string())
+}
+
+
+#[test]
+fn test_sync() {
+    let c = Arc::new(Mutex::new(RefCell::new(1)));
+    let cc = c.clone();
+    thread::spawn(move || {
+        let x = cc.lock().unwrap();
+        let mut xx = x.borrow_mut();
+        *xx +=1;
+    }).join();
+
+    println!("{:?}",c);
 }
