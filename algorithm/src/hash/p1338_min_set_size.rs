ca22592ee7a7eba30ff723940170ba2c5df1b4d1
four <ai-l@outlook.com>
2021-03-31 10:28:48 +0800

    update

--- algorithm/src/hash/p1338_min_set_size.rs
+++ algorithm/src/hash/p1338_min_set_size.rs
@@ -1,24 +1,33 @@
 use std::collections::HashMap;
 
 use crate::Solution;
+use std::ops::Add;
 
 impl Solution {
     pub fn min_set_size(arr: Vec<i32>) -> i32 {
         let mut map = HashMap::new();
-
-        let mut rm = 0;
-        for i in arr {
-            let c = map.entry(i).or_insert(0);
+        for i in arr.iter() {
+            let c = map.entry(*i).or_insert(0);
             *c += 1;
         }
 
-        let mut v = map.iter().collect::<Vec<(i32,i32)>>();
-
-        v.sort_by(|k1,k2| -> k1.0.cmp(k2));
-        let count = 0;
-        for (k,n) in v {
-
+        let mut v: Vec<(i32, i32)> = map.into_iter().collect();
+        v.sort_by(|k1, k2| k2.1.cmp(&k1.1));
+        let mut count = 0;
+        let mut rm = 0;
+        for (_, v) in v {
+            count += v;
+            rm +=1;
+            if count >= (arr.len()/2) as i32  {
+                break;
+            }
         }
-        1
+        rm
     }
+}
+
+#[test]
+fn _test() {
+    let r = Solution::min_set_size(vec![1, 1, 2, 2, 3]);
+    println!("{}", r);
 }
