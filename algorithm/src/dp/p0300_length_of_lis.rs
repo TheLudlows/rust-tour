16eb027681462b650d3dba15934df344c34f3dea
four <ai-l@outlook.com>
2021-05-15 00:06:29 +0800

    add memory search

--- algorithm/src/dp/p0300_length_of_lis.rs
+++ algorithm/src/dp/p0300_length_of_lis.rs
@@ -16,4 +16,27 @@ impl Solution {
         }
         result
     }
+    pub fn length_of_lis2(nums: Vec<i32>) -> i32 {
+        find(&nums, nums.len())
+    }
+}
+fn find(nums:&Vec<i32>, idx:usize) -> i32 {
+    if idx == 0 {
+        return 0;
+    }
+    let mut ret = 0;
+    for i in 0..idx {
+        ret = std::cmp::max(ret, find(nums, i));
+
+        if nums[idx-1] > nums[i] {
+           ret = std::cmp::max(ret, 1 + find(nums, i));
+       }
+    }
+    ret
+}
+#[test]
+fn test() {
+    let v = vec![10,9,2,5,3,7,101,18];
+    let r = Solution::length_of_lis2(v);
+    println!("{}", r)
 }
