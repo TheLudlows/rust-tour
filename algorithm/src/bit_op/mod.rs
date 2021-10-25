d4d115a2da214d5d801199ad4e406e6b62bc65eb
four <gean.lc@alibaba-inc.com>
2021-10-25 15:27:38 +0800

    update

--- algorithm/src/bit_op/mod.rs
+++ algorithm/src/bit_op/mod.rs
@@ -2,3 +2,4 @@ mod p1863_subset_xor_sum;
 mod p1720_decode;
 mod p1601_swap_numbers;
 mod p0338_count_bits;
+mod p0191_hammingWeight;
---
+++ algorithm/src/bit_op/p0191_hammingWeight.rs
@@ -0,0 +1,12 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn hammingWeight(mut n: u32) -> i32 {
+        let mut ret = 0;
+        while n != 0 {
+            ret += 1;
+            n = n & (n - 1);
+        }
+        ret
+    }
+}
